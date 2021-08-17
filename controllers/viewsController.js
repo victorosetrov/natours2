5dc69abac9483a5f3bd25e510715a490b2457208
Viktor Osetrov <victorosetrov@gmail.com>
2021-08-17 22:38:08 +0100

    View2.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,22 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.find({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
